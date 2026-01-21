49d3eb808964ad005c650fb8efce02ab1c7ed003
KevinOrange-Pitt <KEO75@pitt.edu>
2026-01-21 11:27:55 -0500

    Update and rename Test2.java to Weird.java

--- Test2.java
+++
@@ -1,5 +0,0 @@
-public class Test2 {
-    public Test2() {
-        System.out.println("This is wierd.");
-    }
-}
---
+++ Weird.java
@@ -0,0 +1,9 @@
+public class Weird {
+    public Weird() {
+        System.out.println("This is Weird");
+    }
+
+    public static void main(String[] args) {
+        new Weird();
+    }
+}
